e4c23bc87f5bf9628d4da7328e9b2e234da9b6e2
Juan Diego <jroque@frba.utn.edu.ar>
2026-04-14 07:57:21 -0300

    inicio edukids

---
+++ edukids/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('edukids');
+}
